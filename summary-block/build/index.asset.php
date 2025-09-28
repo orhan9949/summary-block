f80e10fa0a209ece9dc507772656ab2efc6ecee3
orhan9949 <47855426+orhan9949@users.noreply.github.com>
2025-09-28 16:45:37 +0300

    Add files via upload

---
+++ summary-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9d0cbf3d18abcdc2777e');
---
+++ summary-block/src/edit.js
@@ -0,0 +1,66 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+// import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+export default function Edit() {
+	const blockProps = useBlockProps( {
+		className: 'summary-block',
+	} );
+
+	// const LIST = [
+	// 	[ 'core/columns', {},
+	// 		[
+	// 			[ 'core/column', {},
+	// 				[
+	// 					[ 'core/list', {},
+	// 						[
+	// 							[ 'core/list-item', { placeholder : 'Text...' } ]
+	// 						]
+	// 					],
+	// 				]
+	// 			]
+	// 		]
+	// 	],
+	// ]
+	const LIST = [ 'core/list' ]
+
+	return (
+		<div { ...blockProps }>
+		 	<p className='summary-block__title' >Summary</p>
+			<div className='summary-block__description'>
+				<InnerBlocks
+					template={ LIST }
+					templateLock="all"/>
+			</div>
+		</div>
+	);
+
+}
+
